bee9314337a7eb1ee34429a0ef624dc9377600ed
Kevin <nivek.sayz@gmail.com>
2022-10-07 22:03:05 -0500

    minor updates

--- pages/index.js
+++ pages/index.js
@@ -4,56 +4,25 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex items-center w-full h-screen text-white bg-gradient-to-b from-[#000203] to-[#000B14]">
-      <div className="flex flex-col w-full max-w-2xl m-auto">
-        <div className="px-4 py-2 ml-auto transition cursor-pointer hover:opacity-80">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-5 h-5"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M10.343 3.94c.09-.542.56-.94 1.11-.94h1.093c.55 0 1.02.398 1.11.94l.149.894c.07.424.384.764.78.93.398.164.855.142 1.205-.108l.737-.527a1.125 1.125 0 011.45.12l.773.774c.39.389.44 1.002.12 1.45l-.527.737c-.25.35-.272.806-.107 1.204.165.397.505.71.93.78l.893.15c.543.09.94.56.94 1.109v1.094c0 .55-.397 1.02-.94 1.11l-.893.149c-.425.07-.765.383-.93.78-.165.398-.143.854.107 1.204l.527.738c.32.447.269 1.06-.12 1.45l-.774.773a1.125 1.125 0 01-1.449.12l-.738-.527c-.35-.25-.806-.272-1.203-.107-.397.165-.71.505-.781.929l-.149.894c-.09.542-.56.94-1.11.94h-1.094c-.55 0-1.019-.398-1.11-.94l-.148-.894c-.071-.424-.384-.764-.781-.93-.398-.164-.854-.142-1.204.108l-.738.527c-.447.32-1.06.269-1.45-.12l-.773-.774a1.125 1.125 0 01-.12-1.45l.527-.737c.25-.35.273-.806.108-1.204-.165-.397-.505-.71-.93-.78l-.894-.15c-.542-.09-.94-.56-.94-1.109v-1.094c0-.55.398-1.02.94-1.11l.894-.149c.424-.07.765-.383.93-.78.165-.398.143-.854-.107-1.204l-.527-.738a1.125 1.125 0 01.12-1.45l.773-.773a1.125 1.125 0 011.45-.12l.737.527c.35.25.807.272 1.204.107.397-.165.71-.505.78-.929l.15-.894z"
-            />
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-            />
-          </svg>
+      <div className="flex flex-col w-full m-4 max-w-lg p-8 mx-auto bg-black border border-[#1C1C1C] rounded-lg">
+        <div className="flex gap-8 py-4 m-auto">
+          <div className="flex">Swap</div>
+            <div className="flex">Limit</div>
         </div>
-        <div className="w-full align-middle items-center sm:flex bg-[#232844] gap-4 mb-4 p-4 rounded-xl ">
-          <span class="absolute flex items-center pl-5 mr-5">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-4 h-4"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M15.59 14.37a6 6 0 01-5.84 7.38v-4.8m5.84-2.58a14.98 14.98 0 006.16-12.12A14.98 14.98 0 009.631 8.41m5.96 5.96a14.926 14.926 0 01-5.841 2.58m-.119-8.54a6 6 0 00-7.381 5.84h4.8m2.581-5.84a14.927 14.927 0 00-2.58 5.84m2.699 2.7c-.103.021-.207.041-.311.06a15.09 15.09 0 01-2.448-2.448 14.9 14.9 0 01.06-.312m-2.24 2.39a4.493 4.493 0 00-1.757 4.306 4.493 4.493 0 004.306-1.758M16.5 9a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0z"
-              />
-            </svg>
-          </span>
+        <div className="px-4 py-2 ml-auto transition cursor-pointer hover:opacity-80"></div>
+        <div className="w-full align-middle items-center sm:flex bg-[#0C0C0C] border border-[#1C1C1C] gap-4  p-4 rounded-xl ">
+          <span class="absolute flex items-center pl-5 mr-5"></span>
           <input
-            className="w-full pl-12 sm:w-1/3 bg-[#171B33] mb-2 sm:mb-0 p-4 rounded-xl"
-            placeholder="WETH"
+            className="w-full sm:w-2/3 bg-[#0C0C0C] text-white mb-2 sm:mb-0 p-4 rounded-xl focus:ring-0 focus:ring-offset-0 focus:outline-none"
+            placeholder="300"
           />
 
           <input
-            className="w-full rounded-xl drop-shadow-lg mb-2 sm:mb-0 p-4 sm:w-2/3 bg-[#0D0F1F]"
+            className="w-full rounded-xl drop-shadow-lg mb-2 sm:mb-0 p-4 sm:w-1/3 bg-[#0C0C0C]"
             placeholder="123"
           />
         </div>
-        <div className="items-center p-2 m-auto mb-4 border border-gray-400 rounded cursor-pointer">
+        <div className="items-center p-2 m-auto border border-gray-400 rounded cursor-pointer">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -70,7 +39,7 @@ export default function Home() {
           </svg>
         </div>
 
-        <div className="w-full align-middle items-center sm:flex bg-[#232844] gap-4 mb-4 p-4 rounded-xl ">
+        <div className="w-full align-middle items-center sm:flex bg-[#0C0C0C] border border-[#1C1C1C] gap-4  p-4 rounded-xl ">
           <span class="absolute flex items-center pl-5 mr-5">
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -88,21 +57,21 @@ export default function Home() {
             </svg>
           </span>
           <input
-            className="w-full sm:w-1/3 mb-2 sm:mb-0 pl-12 bg-[#171B33] p-4 rounded-xl"
+            className="w-full sm:w-1/3 mb-2 sm:mb-0 pl-12 bg-[#0C0C0C] p-4 rounded-xl"
             placeholder="FTM"
           />
 
           <input
-            className="w-full drop-shadow-lg rounded-xl mb-2 sm:mb-0 p-4 sm:w-1/3 bg-[#0D0F1F]"
+            className="w-full drop-shadow-lg rounded-xl mb-2 sm:mb-0 p-4 sm:w-1/3 bg-[#0C0C0C]"
             placeholder="test"
           />
           <input
-            className="w-full drop-shadow-lg rounded-xl mb-2 sm:mb-0 p-4 text-center sm:w-1/3 bg-[#0D0F1F]"
+            className="w-full drop-shadow-lg rounded-xl mb-2 sm:mb-0 p-4 text-center sm:w-1/3 bg-[#0C0C0C]"
             placeholder="test"
           />
         </div>
-        <div className="px-16 py-2 mx-auto mt-4 text-xs font-bold text-center uppercase transition bg-blue-500 rounded cursor-pointer hover:opacity-80">
-          submit
+        <div className="px-16 w-full py-4 mx-auto mt-4 text-xs font-bold text-center transition rounded-xl cursor-pointer bg-gradient-to-r from-[#344DBF] to-[#3098FF] hover:opacity-80">
+          Swap
         </div>
       </div>
     </div>
