338a938e5af7ce47c413d25f3d0f06ef9679ca0f
lucasregueiroo <hello@lucasregueiro.com>
2022-11-24 02:59:36 +0100

    wireframe cleanup

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,9 @@
 import '../styles/globals.css'
-import Navbar from "../components/navbar"
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div className="bg-[url('/static/images/background.svg')] bg-no-repeat bg-cover min-h-screen font-DMSans">
-      <Navbar />
-      <Component {...pageProps} />
-    </div>
+    <Component {...pageProps} />
+
   );
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
 import Swap from "../components/swap";
+import Navbar from "../components/navbar";
 
 export default function Home() {
   return (
-      <div className="flex justify-center w-full text-white">
+    <div className="bg-[url('/static/images/background.svg')] bg-no-repeat bg-cover min-h-screen font-DMSans">
+      <Navbar />
+<div className="flex justify-center w-full text-white">
         <Swap />
       </div>
+    </div>
   );
 }
