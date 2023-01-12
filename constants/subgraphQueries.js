74ef7311d07a413ae72edbb09fc62b72f692e6d9
0xnexusflip <0xnexusflip@gmail.com>
2023-01-12 11:54:13 +0000

    add query constants

---
+++ constants/subgraphQueries.js
@@ -0,0 +1,92 @@
+import { gql } from '@apollo/client';
+
+export const POSITIONS_QUERY = gql`
+    {
+        positions(
+            orderBy: createdAtTimestamp
+            orderDirection: desc
+            where: { createdBy: "${account.toLowerCase()}" }
+        ) {
+            id
+            inAmount
+            inToken {
+                id
+                name
+                symbol
+                decimals
+            }
+            liquidity
+            lower
+            outAmount
+            outToken {
+                id
+                name
+                symbol
+                decimals
+            }
+            owner
+            pool {
+                factory
+                id
+                inputPool
+                token0 {
+                    id
+                    name
+                    symbol
+                    decimals
+                }
+                token1 {
+                    id
+                    name
+                    symbol
+                    decimals
+                }
+            }
+            txnHash
+            upper
+        }
+    }
+    `;
+
+export const POOLS_QUERY = gql`
+    {
+        hedgePools(
+            orderBy: id
+            orderDirection: desc
+        ) {
+            factory
+            id
+            inputPool
+            token0 {
+                id
+                name
+                symbol
+                decimals
+            }
+            token1 {
+                id
+                name
+                symbol
+                decimals
+            }
+        }
+    }
+    `;
+
+export const TOKENS_QUERY = gql`
+    {
+        tokens(
+            orderBy: id
+            orderDirection: desc
+        ) {
+            id
+            name
+            symbol
+            decimals
+        }
+    }
+    `;
+
+
+
+
