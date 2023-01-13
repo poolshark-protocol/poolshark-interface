8ee9932067fec262cab15a55a72f65d54cb6829b
0xnexusflip <0xnexusflip@gmail.com>
2023-01-13 02:45:28 +0000

    fix BigNumber

--- hooks/useInputBox.js
+++ hooks/useInputBox.js
@@ -1,21 +1,19 @@
 import { useState } from 'react';
-import { ethers } from "ethers";
+import { BigNumber, ethers } from "ethers";
 
 export default function useInputBox() {
 
-    const [input, setInput] = useState("");
-    const [bnInput, setBnInput] = useState();
+    const [input, setInput] = useState(BigNumber.from("0"));
+    const [bnInput, setBnInput] = useState(BigNumber.from("0"));
     
     const handleChange = event => {
-        //const valueToBn = ethers.utils.parseUnits(event.target.value, 0);
-        //const result = event.target.value.replace(/\D/g, '');
         const result = event.target.value.replace(/[^0-9\.|\,]/g, '')
         setInput(result);
         console.log('value is:', result);
         if (result !== "") {
             const valueToBn = ethers.utils.parseUnits(result, 18);
             setBnInput(valueToBn);
-        }
+          }
     };
 
     const inputBox = () => {
