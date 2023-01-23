978bee91e64f8dc4a64314e48b14a49aa39215e2
0xnexusflip <0xnexusflip@gmail.com>
2023-01-23 23:35:13 +0000

    add CoinListButton

---
+++ components/Buttons/CoinListButton.js
@@ -0,0 +1,25 @@
+function CoinListButton({chooseToken, coin}) {
+    return(
+        <div>
+            <button onClick={chooseToken}
+                data-name={coin.name}
+                data-logouri={coin.logoURI}
+                data-symbol={coin.symbol}
+                data-address={coin.address}
+                key={coin.id}
+            >
+                <img onClick={chooseToken}
+                    data-name={coin.name}
+                    data-logouri={coin.logoURI}
+                    data-decimals={coin.decimals}
+                    data-symbol={coin.symbol}
+                    data-address={coin.id} alt="?"
+                    src={coin.logoURI} 
+                />
+                
+            </button>
+        </div>
+    );
+}
+
+export default CoinListButton
