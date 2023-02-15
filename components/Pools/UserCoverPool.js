a748c3d9c9955abeb33bb194e7cb38b8a8b05ed6
0xnexusflip <0xnexusflip@gmail.com>
2023-02-16 02:15:09 +0700

    add address pasting (need Copy button)

--- components/Pools/UserCoverPool.js
+++ components/Pools/UserCoverPool.js
@@ -77,11 +77,15 @@ useEffect
       >
         <div className="flex gap-x-10 px-4 text-[#646464] my-2">
         <div>
-        <h1 className="text-xs">{tokenOneName} <span>{tokenOneDisplay}</span></h1>
-        <h1 className="text-xs mt-2">{tokenZeroName} <span>{tokenZeroDisplay}</span></h1>
+        <h1 className="text-xs" 
+        onClick={() => {navigator.clipboard.writeText(tokenOneAddress)}}>{tokenOneName} <span>{tokenOneDisplay}</span></h1>
+        <h1 className="text-xs mt-2"
+        onClick={() => {navigator.clipboard.writeText(tokenZeroAddress)}}>{tokenZeroName} <span>{tokenZeroDisplay}</span></h1>
         </div>
        
-          <h1 className="text-xs">Pool: <span>{poolDisplay}</span></h1>
+          <h1 className="text-xs"
+          onClick={() => {navigator.clipboard.writeText(poolAddress)}}
+          >Pool: <span>{poolDisplay}</span></h1>
         </div>
         
         <div className="bg-dark text-sm py-1 text-center rounded-br-xl border-t-grey1 border-t mt-3 rounded-b-xl">
