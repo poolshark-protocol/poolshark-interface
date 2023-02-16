11a7e473ff7fc74438fceed94531beeb08431879
0xnexusflip <0xnexusflip@gmail.com>
2023-02-16 22:31:09 +0700

    add goerli guard to MaxButton

--- components/Buttons/MaxButton.js
+++ components/Buttons/MaxButton.js
@@ -1,11 +1,19 @@
-import { useAccount } from "wagmi";
+import { useAccount, useProvider } from "wagmi";
 import useInputBox from "../../hooks/useInputBox";
+import { chainIdsToNamesForGitTokenList } from "../../utils/chains";
 
 export default function MaxButton(balance0) {
     const [maxBalance] = useInputBox();
+
     const { isConnected } = useAccount();
 
-    if (isConnected) {
+    const {
+        network: { chainId }, chainId: chainIdFromProvider
+      } = useProvider();
+    
+    const chainName = chainIdsToNamesForGitTokenList[chainId]
+
+    if (isConnected && chainName === "goerli") {
         return (
             <button
                 className="flex text-xs uppercase text-[#C9C9C9]"
