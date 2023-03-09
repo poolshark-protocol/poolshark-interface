2bd39e6d39f9060f057a30e9295670580b66fb96
alphak3y <alphak3y@protonmail.com>
2023-03-09 14:59:19 -0700

    new contract addresses

--- constants/contractAddresses.js
+++ constants/contractAddresses.js
@@ -1,9 +1,9 @@
 //TODO: pull from JSON file and have mapping entry for each network
 //"created": "2023-01-15T22:43:49.430Z"
-export const coverPoolAddress = "0xCC4C513D3B2A0B765141031C012C5cafFbD0aF47"
+export const coverPoolAddress = "0xEc62553Af1973A8D87198f11f51e76e180502DDB"
 
-export const tickMathAddress = "0x78161775EdbA0E405C35569D4F9bddB5ddbEe57e"
+export const tickMathAddress = "0x02f821c66bF2466B3Ef3Fc2c962B392d0EE457F4"
 
-export const tokenZeroAddress = "0x52167F1EC8940e5b8FC642bB86F3c0C6F0A4899D"
+export const tokenZeroAddress = "0x8Fa1Fdd860E3c56dAfD09a048FfDA4965376945e"
 
-export const tokenOneAddress  = "0xdcF62D25FD6aD48277989E93827fD9CCf650975f"
+export const tokenOneAddress  = "0xc3a0736186516792C88e2c6d9B209471651Aa46E"
