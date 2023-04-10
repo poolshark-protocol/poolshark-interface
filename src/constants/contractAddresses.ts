28ff3283c3bdef5b0e2fb52c2d4bca1aadb880ca
alphak3y <alphak3y@protonmail.com>
2023-04-10 11:41:18 -0600

    deploy new range pool with same tokens as cover pool; update addresses and abis

--- src/constants/contractAddresses.ts
+++ src/constants/contractAddresses.ts
@@ -4,17 +4,16 @@
 // cover pools
 export const tokenZeroAddress   = "0x829e4a03A5Bd1EC5b6f5CC1d3A77c8e54A294847" // WETH
 export const tokenOneAddress    = "0xf853592f1e4ceA2B5e722A17C6f917a4c70d40Ca" // DAI
-export const coverPoolAddress = "0x770eE71b7961D155F7F6B6356c08820764f01070"
+export const coverPoolAddress   = "0x770eE71b7961D155F7F6B6356c08820764f01070"
 export const coverPoolFactoryAddress = "0xC7F9348F005508f5354B4f5753C2F3C948B889F8"
 
 // range pools
-export const rangeTokenZero   = "0x7024879Eda80577Cbc0Cb039ad3c7081f38ABb41"
-export const rangeTokenOne    = "0xA826F06C47597549faa74D8916EF4Da8f417F16c"
-export const rangePoolAddress = "0x39C459ed1EFe9f2959CF0a249fB964fE81Adf5D3"
-export const rangePoolFactoryAddress = "0x58d8235108E12E6B725A53B57CD0b00C5eDeE0da"
+export const rangePoolAddress = "0xe35bb5D6304c132112D5bB6625F723209A45c099"
+export const rangePoolFactoryAddress = "0x7Ffc8AdF56f941cf0b85152ec76dB93c7fD07324"
 
 // libraries
 export const tickMathAddress = "0x02f821c66bF2466B3Ef3Fc2c962B392d0EE457F4"
+export const rangePoolSamplesAddress = "0x06A1c422bC002935AF75Da3ACa9C23205353a88b"
 
 //TODO: pull addresses remotely so new networks can be added easily
 
