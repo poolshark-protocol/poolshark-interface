fd6c4e98949a2269723f7ef91a1455724fe12477
0xnexusflip <0xnexusflip@gmail.com>
2023-04-18 04:41:19 +0900

    add price and quotes helpers

--- src/utils/getPriceAndQuotes.tsx
+++ src/utils/getPriceAndQuotes.tsx
@@ -1,8 +1,9 @@
 import { useContractRead } from "wagmi";
 import { BigNumber, ethers } from "ethers";
-import { coverPoolAddress } from "../constants/contractAddresses";
+import { coverPoolAddress, rangePoolAddress } from "../constants/contractAddresses";
 import { coverPoolABI } from '../abis/evm/coverPool'
 import { useState } from "react";
+import { rangePoolABI } from "../abis/evm/rangePool";
 
 export function getCoverQuoteWagmi(
     zeroForOne: boolean, 
@@ -39,6 +40,60 @@ export function getCoverPriceWagmi(zeroForOne: boolean) {
   useContractRead({
       address: coverPoolAddress,
       abi: coverPoolABI,
+      functionName: zeroForOne ? "pool1" : "pool0",
+      args: [],
+      chainId: 421613,
+      watch: true,
+      onSuccess(data) {
+        console.log("Success", data);
+        setPrice(data)
+      },
+      onError(error) {
+        console.log("Error", error);
+      },
+      onSettled(data, error) {
+        console.log("Settled", { data, error });
+      },
+    });
+
+    return price[0];
+}
+
+export function getRangeQuoteWagmi(
+    zeroForOne: boolean, 
+    amountIn: BigNumber, 
+    priceLimit: BigNumber
+    ) {
+    const [quote, setQuote] = useState(null);
+
+    useContractRead({
+        address: rangePoolAddress,
+        abi: rangePoolABI,
+        functionName: "quote",
+        args: [true, ethers.utils.parseUnits('1', 18), BigNumber.from('4295128739')],
+        chainId: 421613,
+        watch: true,
+        onSuccess(data) {
+          console.log("Success", data);
+          setQuote(data)
+        },
+        onError(error) {
+          console.log("Error", error);
+        },
+        onSettled(data, error) {
+          console.log("Settled", { data, error });
+        },
+      });
+    
+      return quote;  
+}
+
+export function getRangePriceWagmi(zeroForOne: boolean) {
+  const [price, setPrice] = useState(null);
+
+  useContractRead({
+      address: rangePoolAddress,
+      abi: rangePoolABI,
       functionName: zeroForOne ? "pool1" : "pool0",
       args: [],
       chainId: 421613,
