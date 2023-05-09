ec35a5473813ef2ff476b7d01c11c02737c886f9
alphak3y <alphak3y@protonmail.com>
2023-05-09 06:22:00 -0600

    arb goerli deployment

--- src/constants/contractAddresses.ts
+++ src/constants/contractAddresses.ts
@@ -6,18 +6,15 @@ export const tokenZeroAddress   = "0x6774be1a283Faed7ED8e40463c40Fb33A8da3461" /
 export const tokenOneAddress    = "0xC26906E10E8BDaDeb2cf297eb56DF59775eE52c4" // DAI
 
 // cover pools
-export const coverPoolAddress   = "0xB84Ab81B3d405128ce09FB72A88fea4929E364Ce"
-export const coverPoolFactoryAddress = "0xC7F9348F005508f5354B4f5753C2F3C948B889F8"
+export const coverPoolAddress   = "0xC0c4E0AA5a413373CACBC39AE5e6597D78d26325"
+export const coverPoolFactoryAddress = "0xaD1Aff1234193c1B0A8A178fDe27Fd786b99B7b3"
 
 // range pools
-export const rangePoolAddress = "0x3b2ec00a3b33661dd118ce9666be9cacccfad887"
-export const rangePoolFactoryAddress = "0x1598dce2d358f652e6625039c3c12e63b0444bef"
+export const rangePoolAddress = "0x8d35973D1c806EBb7408843654f84cB10fd21621"
+export const rangePoolFactoryAddress = "0x50475D93B8E374F4E34d910cb9696392a435d4F6"
 
 // libraries
-export const tickMathAddress = "0x02f821c66bF2466B3Ef3Fc2c962B392d0EE457F4"
-export const rangePoolSamplesAddress = "0x06A1c422bC002935AF75Da3ACa9C23205353a88b"
+export const tickMathAddress = "0xC0A5e34d581c62c8D62ADa24A304E67A20DF9f45"
+export const rangePoolSamplesAddress = "0x83E8d1b99111c393Be0d7537561d1D70C3280d2b"
 
 //TODO: pull addresses remotely so new networks can be added easily
-
-
-
