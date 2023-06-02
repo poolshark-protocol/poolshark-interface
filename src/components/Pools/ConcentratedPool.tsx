13e1e2c0e2100edcfb63ac00c6775cf506c9305d
alphak3y <alphak3y@protonmail.com>
2023-06-02 10:27:04 -0600

    decimal point

--- src/components/Pools/ConcentratedPool.tsx
+++ src/components/Pools/ConcentratedPool.tsx
@@ -738,7 +738,7 @@ export default function ConcentratedPool({
                       (document.getElementById('maxInput') as HTMLInputElement)
                         ?.value
                           .replace(/^0+(?=[^.0-9]|$)/, match => match.length > 1 ? '0' : match)
-                          .replace(/^(\.)+/, '0')
+                          .replace(/^(\.)+/, '0.')
                           .replace(/(?<=\..*)\./g, '')
                           .replace(/^0+(?=\d)/, '')
                           .replace(/[^\d.]/g, '')
