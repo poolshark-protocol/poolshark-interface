3bee8411245d3a430708f07be5c93daa9f90fd2f
alphak3y <alphak3y@protonmail.com>
2023-07-04 10:25:51 -0600

    tick spacing as hook dependency

--- src/components/Pools/ConcentratedPool.tsx
+++ src/components/Pools/ConcentratedPool.tsx
@@ -259,7 +259,7 @@ export default function ConcentratedPool({
       )
     }
     setAmounts()
-  }, [lowerPrice, upperPrice])
+  }, [lowerPrice, upperPrice, tickSpacing])
 
   async function updateGasFee() {
     if (
