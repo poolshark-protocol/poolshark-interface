283cab5c07a249b9b49198950e4d72a228d7926f
0xnexusflip <0xnexusflip@gmail.com>
2023-08-09 14:35:36 +0100

    add zustand hook, graph queries & token for limit

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -9,6 +9,17 @@ export type tokenCover = {
   coverUSDPrice: number
 }
 
+export type tokenLimit = {
+  callId: number
+  name: string
+  symbol: string
+  logoURI: string
+  address: `0x${string}`
+  userBalance: number
+  userPoolAllowance: number
+  limitUSDPrice: number
+}
+
 export type token = {
   callId: number
   name: string
