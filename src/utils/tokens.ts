b9161075c12bba434578689c73286b61550689ee
re traca <ggpt98@gmail.com>
2023-08-14 13:57:26 +0100

    token20A as DAI

--- src/utils/tokens.ts
+++ src/utils/tokens.ts
@@ -3,7 +3,7 @@
 import { TickMath, invertPrice } from "./math/tickMath";
 
 export const logoMap = {
-  TOKEN20A: "/static/images/token.png",
+  TOKEN20A: "/static/images/dai_icon.png",
   TOKEN20B: "/static/images/eth_icon.png",
   USDC: "/static/images/token.png",
   WETH: "/static/images/eth_icon.png",
