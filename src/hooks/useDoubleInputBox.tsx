29fd9e6c4f1dbce29516a03bfb3e0fe0ed5001e6
0xnexusflip <0xnexusflip@gmail.com>
2023-08-17 14:17:58 +0100

    finish double input box hook

--- src/hooks/useDoubleInputBox.tsx
+++ src/hooks/useDoubleInputBox.tsx
@@ -11,69 +11,109 @@ export default function useDoubleInputBox() {
   const [inputLower, setInputLower] = useState(BigNumber.from('0'))
   const [bnInputLower, setBnInputLower] = useState(BigNumber.from('0'))
 
-  const handleUpperChange = (event, updateValue) => {
+  const handleUpperChange = (event, updateValue, inputLower) => {
     const result = inputFilter(event.target.value)
     //TODO: do not allow for exceeding max decimals
     setDisplayUpper(result == '' ? '' : result)
+    setDisplayLower(result == '' ? '' : inputLower)
+
     if (result == '') {
       setBnInputUpper(BigNumber.from('0'))
+      setBnInputLower(BigNumber.from('0'))
     }
     if (updateValue !== undefined) {
       updateValue(result == '' ? 0 : result)
+
       setInputUpper(
         result == ''
           ? BigNumber.from('0')
           : ethers.utils.parseUnits(result, 18),
       )
+      setInputLower(
+        result == ''
+        ? BigNumber.from('0')
+        : ethers.utils.parseUnits(inputLower, 18),
+      )
       if (result !== '') {
         const valueToBn = ethers.utils.parseUnits(result, 18)
+        const valueLowerToBn = ethers.utils.parseUnits(inputLower, 18)
+
         setBnInputUpper(valueToBn)
+        setBnInputLower(valueLowerToBn)
       }
     }
     setInputUpper(
       result == '' ? BigNumber.from('0') : ethers.utils.parseUnits(result, 18),
     )
+    setInputLower(
+        result == '' ? BigNumber.from('0') : ethers.utils.parseUnits(inputLower, 18),
+    )
+
     if (result !== '') {
       const valueToBn = ethers.utils.parseUnits(result, 18)
+      const valueLowerToBn = ethers.utils.parseUnits(inputLower, 18)
+
       setBnInputUpper(valueToBn)
+      setBnInputLower(valueLowerToBn)
     }
   }
 
-  const handleLowerChange = (event, updateValue) => {
-    const result = event.target.value.replace(/[^\d.]/g, '')
+  const handleLowerChange = (event, updateValue, inputUpper) => {
+    const result = inputFilter(event.target.value)
     //TODO: do not allow for exceeding max decimals
     setDisplayLower(result == '' ? '' : result)
+    setDisplayUpper(result == '' ? '' : inputUpper)
+
+    if (result == '') {
+        setBnInputLower(BigNumber.from('0'))
+        setBnInputUpper(BigNumber.from('0'))
+    }
     if (updateValue !== undefined) {
       updateValue(result == '' ? 0 : result)
+
       setInputLower(
         result == ''
           ? BigNumber.from('0')
           : ethers.utils.parseUnits(result, 18),
       )
+      setInputUpper(
+        result == ''
+        ? BigNumber.from('0')
+        : ethers.utils.parseUnits(inputUpper, 18),
+      )
       if (result !== '') {
         const valueToBn = ethers.utils.parseUnits(result, 18)
+        const valueUpperToBn = ethers.utils.parseUnits(inputUpper, 18)
+
         setBnInputLower(valueToBn)
+        setBnInputUpper(valueUpperToBn)
       }
     }
 
     setInputLower(
       result == '' ? BigNumber.from('0') : ethers.utils.parseUnits(result, 18),
     )
+    setInputUpper(
+        result == '' ? BigNumber.from('0') : ethers.utils.parseUnits(inputUpper, 18),
+    )
     if (result !== '') {
       const valueToBn = ethers.utils.parseUnits(result, 18)
+      const valueUpperToBn = ethers.utils.parseUnits(inputUpper, 18)
+
       setBnInputLower(valueToBn)
+      setBnInputUpper(valueUpperToBn)
     }
   }
 
   //TODO: add an optional param for changing value
-  const upperInputBox = (placeholder: string, updateValue?: any) => {
+  const upperInputBox = (placeholder: string, updateValue?: any, inputLower?: any) => {
     return (
       <div className="">
         <input
           autoComplete="off"
           type="text"
           id="input"
-          onChange={(e) => handleUpperChange(e, updateValue)}
+          onChange={(e) => handleUpperChange(e, updateValue, inputLower)}
           value={displayUpper}
           placeholder={placeholder}
           className="bg-[#0C0C0C] placeholder:text-grey1 w-full text-white text-2xl mb-2 rounded-xl focus:ring-0 focus:ring-offset-0 focus:outline-none"
@@ -82,14 +122,14 @@ export default function useDoubleInputBox() {
     );
   }
 
-  const lowerInputBox = (placeholder: string, updateValue?: any) => {
+  const lowerInputBox = (placeholder: string, updateValue?: any, inputUpper?: any) => {
     return (
       <div className="flex gap-x-2">
         <input
           autoComplete="off"
           type="text"
           id="LimitInput"
-          onChange={(e) => handleLowerChange(e, updateValue)}
+          onChange={(e) => handleLowerChange(e, updateValue, inputUpper)}
           value={displayLower}
           placeholder={placeholder}
           className="bg-[#0C0C0C] placeholder:text-grey1 text-white text-2xl mb-2 rounded-xl focus:ring-0 focus:ring-offset-0 focus:outline-none"
