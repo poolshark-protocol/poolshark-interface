8f88bc010f2f8d79bd5a3b3239190cf6ae3bba99
re traca <ggpt98@gmail.com>
2023-08-30 16:44:42 +0100

    new token addresses

--- src/constants/contractAddresses.ts
+++ src/constants/contractAddresses.ts
@@ -2,8 +2,8 @@
 // CHAINID: 421613
 
 // tokens
-export const tokenZeroAddress   = "0xC26906E10E8BDaDeb2cf297eb56DF59775eE52c4" // DAI
-export const tokenOneAddress    = "0x6774be1a283Faed7ED8e40463c40Fb33A8da3461"  // WETH
+export const tokenZeroAddress   = "0x0BfaaAfa6e8fB009cD4e2Bd3693F2EEC2d18B053" // WETH
+export const tokenOneAddress    = "0x19beE8e887a5db5cf20A841eb4DAACBCacF14B1b"  // DAI
 
 // cover pools
 export const coverPoolAddress   = "0xC0c4E0AA5a413373CACBC39AE5e6597D78d26325"
