108e25753a1942d054a3b71dd34ae85997a680ff
alphak3y <alphak3y@protonmail.com>
2023-09-20 12:51:38 -0600

    add price utils for limit lps

--- src/utils/math/tickMath.ts
+++ src/utils/math/tickMath.ts
@@ -5,6 +5,8 @@ import { mostSignificantBit } from "./mostSignificantBit"
 import JSBD from 'jsbd'
 import { priceToString, scale } from './priceMath'
 import { BigNumber } from 'ethers'
+import { PrecisionMath } from './precisionMath'
+import { DyDxMath } from './dydxMath'
 
 function mulShift(val: JSBI, mulBy: string): JSBI {
   return JSBI.signedRightShift(JSBI.multiply(val, JSBI.BigInt(mulBy)), JSBI.BigInt(128))
@@ -256,4 +258,94 @@ export abstract class TickMath {
       ? tickHigh
       : tickLow
   }
+
+  public static getNewSqrtPrice(sqrtPrice: JSBI, liquidity: JSBI, amount: JSBI, zeroForOne: boolean, exactIn: boolean): JSBI {
+    if (exactIn) {
+      if (zeroForOne) {
+        const liquidityPadded = JSBI.leftShift(liquidity, JSBI.BigInt(96))
+        return PrecisionMath.mulDivRoundingUp(
+          liquidityPadded,
+          sqrtPrice,
+          JSBI.add(liquidityPadded, JSBI.multiply(sqrtPrice, amount))
+        )
+      } else {
+        return JSBI.add(sqrtPrice, JSBI.divide(JSBI.leftShift(amount, JSBI.BigInt(96)), liquidity))
+      }
+    } else {
+      if (zeroForOne) {
+        return JSBI.subtract(sqrtPrice, PrecisionMath.divRoundingUp(JSBI.leftShift(amount, JSBI.BigInt(96)), liquidity))
+      } else {
+        const liquidityPadded = JSBI.leftShift(liquidity, JSBI.BigInt(96))
+        return PrecisionMath.mulDivRoundingUp(
+          liquidityPadded,
+          sqrtPrice,
+          JSBI.subtract(liquidityPadded, JSBI.multiply(sqrtPrice, amount))
+        )
+      }
+    }
+  }
+
+   /**
+   * Returns the average price given [price range], [zeroForOne], and [amountIn]
+   * @param lowerTick the lower tick of the price range
+   * @param upperTick the upper tick of the price range
+   * @param zeroForOne true if token0 => token1; false if token1 => token0
+   * @param amountIn the amount of token deposited into the LP position
+   */
+   public static getExpectedPrice(lowerTick: number, upperTick: number, zeroForOne: boolean, liquidity: BigNumber, amountIn: BigNumber): number {
+    invariant(
+      lowerTick >= upperTick ||
+      this.MIN_TICK > lowerTick ||
+      this.MAX_TICK < lowerTick ||
+      this.MIN_TICK > upperTick || 
+      this.MAX_TICK < upperTick,
+      'INVALID_TICK_RANGE'
+    )
+
+    const amount = JSBI.BigInt(amountIn.div(2).toString())
+    const liquidityAmount = JSBI.BigInt(liquidity.toString())
+
+    let newSqrtPrice
+    if (zeroForOne) {
+      // start from lower price
+      const lowerSqrtPrice = this.getSqrtRatioAtTick(lowerTick)
+      newSqrtPrice = this.getNewSqrtPrice(lowerSqrtPrice, liquidityAmount, amount, zeroForOne, true)
+    } else {
+      // start from upper price
+      const upperSqrtPrice = this.getSqrtRatioAtTick(upperTick)
+      newSqrtPrice = this.getNewSqrtPrice(upperSqrtPrice, liquidityAmount, amount, zeroForOne, true)
+    }
+    // convert sqrt price to price string
+    return parseFloat(this.getPriceStringAtSqrtPrice(newSqrtPrice))
+  }
+
+  /**
+   * Returns the expected amount out when the range is 100% crossed
+   * @param lowerTick the lower tick of the price range
+   * @param upperTick the upper tick of the price range
+   * @param zeroForOne true if token0 => token1; false if token1 => token0
+   * @param amountIn the amount of token deposited into the LP position
+   */
+     public static getExpectedAmountOut(lowerTick: number, upperTick: number, zeroForOne: boolean, liquidity: BigNumber): BigNumber {
+      invariant(
+        lowerTick >= upperTick ||
+        this.MIN_TICK > lowerTick ||
+        this.MAX_TICK < lowerTick ||
+        this.MIN_TICK > upperTick || 
+        this.MAX_TICK < upperTick,
+        'INVALID_TICK_RANGE'
+      )
+      const liquidityAmount = JSBI.BigInt(liquidity.toString())
+      const lowerSqrtPrice = this.getSqrtRatioAtTick(lowerTick)
+      const upperSqrtPrice = this.getSqrtRatioAtTick(upperTick)
+  
+      let newSqrtPrice
+      if (zeroForOne) {
+        // get amount1 at upper price
+        return BigNumber.from(String(DyDxMath.getDy(liquidityAmount, lowerSqrtPrice, upperSqrtPrice, true)))
+      } else {
+        // get amount0 at lower price
+        return BigNumber.from(String(DyDxMath.getDx(liquidityAmount, lowerSqrtPrice, upperSqrtPrice, true)))
+      }
+    }
 }
