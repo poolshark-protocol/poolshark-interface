29f1500c9c0134655fb9574be523c1c1eed7195c
alphak3y <alphak3y@protonmail.com>
2023-09-20 13:57:57 -0600

    add tests for utils functions

--- tests/tickMath.test.ts
+++ tests/tickMath.test.ts
@@ -1,5 +1,7 @@
 import JSBI from 'jsbi';
 import { TickMath, invertPrice } from '../src/utils/math/tickMath'
+import { getAveragePrice, getExpectedAmountOut } from '../src/utils/math/priceMath';
+import { BigNumber, ethers } from 'ethers';
 
 describe('TickMath tests', () => {
   test('price string of 1.00 converts to tick 0', () => {
@@ -57,5 +59,40 @@ describe('TickMath tests', () => {
     // check priceString
     console.log('minPrice', priceString.toString())
     expect(priceString).toStrictEqual('5.00000e-1')
+  });
+
+  test('position of tick 0 to 100 zeroForOne true gives expected price', () => {
+    const lowerTick = 0
+    const upperTick = 100
+    const price = getAveragePrice(lowerTick, upperTick, true, BigNumber.from('20051041647900280328782'), ethers.utils.parseUnits('100', 18))
+    // check price
+    expect(price).toStrictEqual(1.005)
+  });
+
+  test('position of tick 0 to 100 zeroForOne false gives expected price', () => {
+    const lowerTick = -100
+    const upperTick = 0
+    const price = getAveragePrice(lowerTick, upperTick, false, BigNumber.from('20051041647900280328782'), ethers.utils.parseUnits('100', 18))
+    // check price
+    console.log('average price', price)
+    expect(price).toStrictEqual(0.99503)
+  });
+
+  test.only('position of tick 0 to 100 zeroForOne false gives expected amount out', () => {
+    const lowerTick = 0
+    const upperTick = 100
+    const amountOut = getExpectedAmountOut(lowerTick, upperTick, true, BigNumber.from('20051041647900280328782'))
+    // check amount out
+    console.log('average price', amountOut)
+    expect(amountOut.toString()).toStrictEqual('100501226962305120351')
+  });
+
+  test.only('position of tick 0 to 100 zeroForOne false gives expected amount out', () => {
+    const lowerTick = -100
+    const upperTick = 0
+    const amountOut = getExpectedAmountOut(lowerTick, upperTick, false, BigNumber.from('20051041647900280328782'))
+    // check amount out
+    console.log('average price', amountOut)
+    expect(amountOut.toString()).toStrictEqual('100501226962305120351')
   });
 });
