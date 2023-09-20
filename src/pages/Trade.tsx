c00344a4ab3224a6c1bada834a67e2add33774f9
0xnexusflip <0xnexusflip@gmail.com>
2023-09-20 14:25:42 +0100

    comment out cancel button

--- src/pages/Trade.tsx
+++ src/pages/Trade.tsx
@@ -1114,7 +1114,8 @@ export default function Trade() {
                 if (allLimitPosition.id != undefined) {
                   return (
                     <tr className="text-right text-xs md:text-sm"
-                        key={allLimitPosition.id}>
+                        key={allLimitPosition.id}
+                    >
                       <td className="">
                         <div className="flex items-center text-sm text-grey1 gap-x-2 text-left">
                           <img
@@ -1217,13 +1218,13 @@ export default function Trade() {
             </tbody>
           )}
         </table>
-        {activeOrdersSelected && (
+        {/*{activeOrdersSelected && (
           <div className="flex items-center justify-center w-full mt-9">
             <button className="bg-red-900/20 py-2 px-5 text-xs text-red-600 mx-auto">
               Cancell All Orders
             </button>
           </div>
-        )}
+        )}*/}
       </div>
     </div>
   );
