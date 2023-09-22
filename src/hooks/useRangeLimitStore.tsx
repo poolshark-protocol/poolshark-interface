ea4a9e25b88bac702462bf2012c38eeb6c021afe
alphak3y <alphak3y@protonmail.com>
2023-09-22 12:22:05 -0600

    updated router address

--- src/hooks/useRangeLimitStore.tsx
+++ src/hooks/useRangeLimitStore.tsx
@@ -126,8 +126,8 @@ type RangeLimitAction = {
 
 const initialRangeLimitState: RangeLimitState = {
   poolRouterAddresses: {
-    arbitrumGoerli: "0xb6d2db5c8a7fce230fb32049d0387d0528a0cad5",
-    arbitrumMainnet: "0x379cbea9234cae9e106bc2a86b39610dc56dbae2",
+    arbitrumGoerli: "0xd2a20126829f41185853d13a6be1938b10b984f6",
+    arbitrumMainnet: "", // not deployed yet
   },
   //range pools
   rangePoolAddress: "0x000",
