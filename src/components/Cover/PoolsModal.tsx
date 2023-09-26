8c6ed8846adb7a56ec74b4a032100f57b2020dd4
lucasregueiroo <hello@lucasregueiro.com>
2023-09-26 11:46:43 +0800

    fix cover modal styling

--- src/components/Cover/PoolsModal.tsx
+++ src/components/Cover/PoolsModal.tsx
@@ -204,8 +204,8 @@ export default function PoolsModal({ isOpen, setIsOpen, prefill, setParams }) {
                         </div>
                       </div>
                     ) : (
-                      <div className="overflow-x-scroll lg:overflow-hidden scrollbar pb-3 lg:pb-0">
-                        <div className="w-[900px] lg:w-auto space-y-2">
+                      <div className="pb-3 lg:pb-0">
+                        <div className="w-auto space-y-2">
                           {allRangePositions.map((allLimitPosition) => {
                             if (
                               allLimitPosition.userOwnerAddress ===
