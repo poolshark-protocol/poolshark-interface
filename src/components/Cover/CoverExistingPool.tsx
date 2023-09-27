bed4ca28392b3817a3386415d8174938cc07898b
alphak3y <alphak3y@protonmail.com>
2023-09-27 12:19:28 -0600

    token display is based on token order

--- src/components/Cover/CoverExistingPool.tsx
+++ src/components/Cover/CoverExistingPool.tsx
@@ -513,8 +513,8 @@ export default function CoverExistingPool({ goBack }) {
         <div className="flex justify-between md:justify-start gap-x-4 items-center">
           <button className="flex w-full items-center gap-x-3 bg-black border border-grey md:px-4 px-2 py-1.5 rounded-[4px]">
             <div className="flex md:text-base text-sm items-center gap-x-2 w-full">
-              <img className="md:w-7 w-6" src={tokenOut.logoURI} />
-              {tokenOut.symbol}
+              <img className="md:w-7 w-6" src={(tokenOrder ? tokenOut : tokenIn).logoURI} />
+              {(tokenOrder ? tokenOut : tokenIn).symbol}
             </div>
           </button>
           <ArrowLongRightIcon
@@ -525,8 +525,8 @@ export default function CoverExistingPool({ goBack }) {
           />
           <button className="flex w-full items-center gap-x-3 bg-black border border-grey md:px-4 px-2 py-1.5 rounded-[4px]">
             <div className="flex md:text-base text-sm items-center gap-x-2 w-full">
-              <img className="md:w-7 w-6" src={tokenIn.logoURI} />
-              {tokenIn.symbol}
+              <img className="md:w-7 w-6" src={(tokenOrder ? tokenIn : tokenOut).logoURI} />
+              {(tokenOrder ? tokenIn : tokenOut).symbol}
             </div>
           </button>
         </div>
