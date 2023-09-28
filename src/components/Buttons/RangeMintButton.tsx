27c40ce555cceed5879bb69740e7840fc95b3115
lucasregueiroo <hello@lucasregueiro.com>
2023-09-29 00:24:05 +0800

    add modal for when positions are minted

--- src/components/Buttons/RangeMintButton.tsx
+++ src/components/Buttons/RangeMintButton.tsx
@@ -12,6 +12,7 @@ import { BN_ZERO } from "../../utils/math/constants";
 import { useRangeLimitStore } from "../../hooks/useRangeLimitStore";
 import { poolsharkRouterABI } from "../../abis/evm/poolsharkRouter";
 import { ethers } from "ethers";
+import PositionMintModal from "../Modals/PositionMint";
 
 export default function RangeMintButton({
   disabled,
@@ -103,23 +104,7 @@ export default function RangeMintButton({
       >
         {buttonMessage}
       </button>
-      <div className="fixed bottom-4 right-4 flex flex-col space-y-2 z-50">
-        {errorDisplay && (
-          <ErrorToast
-            hash={data?.hash}
-            errorDisplay={errorDisplay}
-            setErrorDisplay={setErrorDisplay}
-          />
-        )}
-        {isLoading ? <ConfirmingToast hash={data?.hash} /> : <></>}
-        {successDisplay && (
-          <SuccessToast
-            hash={data?.hash}
-            successDisplay={successDisplay}
-            setSuccessDisplay={setSuccessDisplay}
-          />
-        )}
-      </div>
+      <PositionMintModal errorDisplay={errorDisplay} hash={data?.hash} isLoading={isLoading} successDisplay={successDisplay} type={"range"}/>
     </>
   );
 }
