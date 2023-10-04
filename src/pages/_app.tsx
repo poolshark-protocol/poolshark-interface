253be78a084bbea3c9f933146b5a7c7ca5095563
lucasregueiroo <hello@lucasregueiro.com>
2023-10-04 17:12:44 +0100

    change copy

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -396,7 +396,7 @@ function MyApp({ Component, pageProps }) {
             <div className="md:max-w-lg">
                 <img src="/static/images/logo.png" className="mx-auto mb-10 w-56" />
                 <div className="text-white text-center mb-10 text-sm">
-                Poolshark is currently under a closed testnet beta. You must be whitelisted in order to access the platform.  <a href="https://27m2bjslfwm.typeform.com/to/mJZBpT2x" className="text-main2 underline flex items-center justify-center mt-4 gap-x-2">CLick here to whitelist your address<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-4 h-4"><path stroke-linecap="round" stroke-linejoin="round" d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" /></svg></a>
+                Poolshark is currently under a closed testnet beta. You must be whitelisted in order to access the platform.  <a href="https://27m2bjslfwm.typeform.com/to/mJZBpT2x" className="text-main2 underline flex items-center justify-center mt-4 gap-x-2">Click here to join the waitlist<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-4 h-4"><path stroke-linecap="round" stroke-linejoin="round" d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" /></svg></a>
                 </div>
                 <div className="mx-auto text-white text-center">
                 <ConnectWalletButton center={true}/>
@@ -412,7 +412,7 @@ function MyApp({ Component, pageProps }) {
               <div className="md:max-w-lg ">
                 <img src="/static/images/logo.png" className="mx-auto mb-10 w-60" />
                 <div className="text-white text-center mb-10 text-sm">
-                Poolshark is currently under a closed testnet beta. You must be whitelisted in order to access the platform. <a href="https://27m2bjslfwm.typeform.com/to/mJZBpT2x" className="text-main2 underline flex items-center justify-center mt-4 gap-x-2">CLick here to whitelist your address<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-4 h-4">
+                Poolshark is currently under a closed testnet beta. You must be whitelisted in order to access the platform. <a href="https://27m2bjslfwm.typeform.com/to/mJZBpT2x" className="text-main2 underline flex items-center justify-center mt-4 gap-x-2">Click here to join the waitlist<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-4 h-4">
   <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
 </svg>
 </a>
