d208413672280455cb5529a68be8cc6b4e30401f
0xnexusflip <0xnexusflip@gmail.com>
2023-10-05 18:05:50 +0200

    disable limit add liq button

--- src/components/Buttons/LimitAddLiqButton.tsx
+++ src/components/Buttons/LimitAddLiqButton.tsx
@@ -92,7 +92,7 @@ import { ethers } from "ethers";
     return (
       <>
       <button
-        disabled={disabled}
+        disabled={/*disabled*/true}
         className="w-full py-4 mx-auto disabled:cursor-not-allowed cursor-pointer text-center transition rounded-full  border border-main bg-main1 uppercase text-sm disabled:opacity-50 hover:opacity-80"
         onClick={() => write?.()}
       >
