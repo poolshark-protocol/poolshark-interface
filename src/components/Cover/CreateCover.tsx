0ed2cfa2b98d72e7af0eb90a2449ca33e85cf4e5
alphak3y <alphak3y@protonmail.com>
2023-10-04 20:11:23 -0600

    handle price switch

--- src/components/Cover/CreateCover.tsx
+++ src/components/Cover/CreateCover.tsx
@@ -226,7 +226,8 @@ export default function CreateCover(props: any) {
       tickSpread
     );
     const upperPrice = TickMath.getPriceStringAtTick(
-      tokenOrder ? tickAtPrice - tickSpread * 6 : tickAtPrice + tickSpread * 18,
+      tokenOrder ? tickAtPrice - tickSpread * 6 
+                 : tickAtPrice + tickSpread * 18,
       tickSpread
     );
     setLowerPrice(lowerPrice);
@@ -237,8 +238,6 @@ export default function CreateCover(props: any) {
       lowerPrice: lowerPrice,
       upperPrice: upperPrice,
     });
-    setMinInput(lowerPrice);
-    setMaxInput(upperPrice);
   }
 
   ////////////////////////////////Position Price Delta
@@ -246,6 +245,7 @@ export default function CreateCover(props: any) {
   const [upperPrice, setUpperPrice] = useState("0");
 
   useEffect(() => {
+    console.log('setting position data')
     setCoverPositionData({
       ...coverPositionData,
       lowerPrice: lowerPrice,
@@ -491,26 +491,11 @@ export default function CreateCover(props: any) {
 
   /////////////////////Logic for switching price denomination
 
-  const [minInput, setMinInput] = useState(lowerPrice);
-  const [maxInput, setMaxInput] = useState(upperPrice);
-
   const handlePriceSwitch = () => {
+    setLowerPrice(invertPrice(lowerPrice, false));
+    setUpperPrice(invertPrice(upperPrice, false));
     setPriceOrder(!priceOrder);
-    setMaxInput(invertPrice(maxInput, false));
-    setMinInput(invertPrice(minInput, false));
   };
-
-  useEffect(() => {
-    setUpperPrice(invertPrice(maxInput, priceOrder));
-    setLowerPrice(invertPrice(minInput, priceOrder));
-  }, [maxInput, minInput]);
-
-  useEffect(() => {
-    if (lowerPrice !== "0" && upperPrice !== "0") {
-      setMinInput(lowerPrice);
-      setMaxInput(upperPrice);
-    }
-  }, [lowerPrice, upperPrice]);
 
   return (
     <div className="flex flex-col space-y-8">
@@ -618,25 +603,15 @@ export default function CreateCover(props: any) {
             <div className="border bg-black border-grey rounded-[4px] flex flex-col w-full items-center justify-center gap-y-3 h-32">
               <span className="text-grey1 text-xs">MIN. PRICE</span>
               <span className="text-white text-3xl">
-                {priceOrder ? (
+                {(
                   <input
                     autoComplete="off"
                     className="bg-black py-2 outline-none text-center w-full"
                     placeholder="0"
                     id="minInput"
                     type="text"
-                    value={minInput}
-                    onChange={(e) => setMinInput(inputFilter(e.target.value))}
-                  />
-                ) : (
-                  <input
-                    autoComplete="off"
-                    className="bg-black py-2 outline-none text-center w-full"
-                    placeholder="0"
-                    id="minInput"
-                    type="text"
-                    value={maxInput}
-                    onChange={(e) => setMaxInput(inputFilter(e.target.value))}
+                    value={lowerPrice}
+                    onChange={(e) => setLowerPrice(inputFilter(e.target.value))}
                   />
                 )}
               </span>
@@ -644,25 +619,15 @@ export default function CreateCover(props: any) {
             <div className="border bg-black border-grey rounded-[4px] flex flex-col w-full items-center justify-center gap-y-3 h-32">
               <span className="text-grey1 text-xs">MAX. PRICE</span>
               <span className="text-white text-3xl">
-                {priceOrder ? (
+                {(
                   <input
                     autoComplete="off"
                     className="bg-black py-2 outline-none text-center w-full"
                     placeholder="0"
-                    id="minInput"
+                    id="maxInput"
                     type="text"
-                    value={maxInput}
-                    onChange={(e) => setMaxInput(inputFilter(e.target.value))}
-                  />
-                ) : (
-                  <input
-                    autoComplete="off"
-                    className="bg-black py-2 outline-none text-center w-full"
-                    placeholder="0"
-                    id="minInput"
-                    type="text"
-                    value={minInput}
-                    onChange={(e) => setMinInput(inputFilter(e.target.value))}
+                    value={upperPrice}
+                    onChange={(e) => setUpperPrice(inputFilter(e.target.value))}
                   />
                 )}
               </span>
@@ -674,13 +639,14 @@ export default function CreateCover(props: any) {
               onClick={() => setExpanded(!expanded)}
             >
               <div className="flex-none text-xs uppercase text-[#C9C9C9]">
-                {1} {tokenIn.symbol} =
+                {1} {priceOrder ? tokenIn.symbol : tokenOut.symbol} =
                 {" " +
-                  (tokenOut.coverUSDPrice / tokenIn.coverUSDPrice).toPrecision(
+                  (priceOrder ? tokenIn.coverUSDPrice / tokenOut.coverUSDPrice
+                              : tokenOut.coverUSDPrice / tokenIn.coverUSDPrice).toPrecision(
                     5
                   ) +
                   " " +
-                  tokenOut.symbol}
+                  (priceOrder ? tokenOut.symbol : tokenIn.symbol)}
                 {/* {!tokenIn.coverUSDPrice
                   ? "?" + " " + tokenOut.symbol
                   : (tokenOrder
