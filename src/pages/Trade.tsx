577ea3e88b40a181831771dccb904cf62abe644c
lucasregueiroo <hello@lucasregueiro.com>
2023-10-11 16:24:30 +0100

    add styling for 0 limit orders

--- src/pages/Trade.tsx
+++ src/pages/Trade.tsx
@@ -1080,8 +1080,9 @@ export default function Trade() {
             </button>
           </div>
         </div>
-        <table className="w-full table-auto divide-y divide-grey border border-grey mt-3 bg-dark w-full w-full rounded-[4px]">
-          <thead className="h-10">
+        <div className="overflow-hidden rounded-[4px] mt-3 bg-dark  border border-grey">
+        <table className="w-full table-auto rounded-[4px]">
+          <thead className={`h-10 ${allLimitPositions.length === 0 && "hidden"}`}>
             <tr className="text-[11px] text-grey1/90 mb-3 leading-normal">
               <th className="text-left pl-3 uppercase">Sell</th>
               <th className="text-left uppercase">Buy</th>
@@ -1090,7 +1091,22 @@ export default function Trade() {
               <th className="text-left md:table-cell hidden pl-2 uppercase">Age</th>
             </tr>
           </thead>
-          {activeOrdersSelected ? (
+          {allLimitPositions.length === 0  ? (<td className="text-grey1 text-xs w-full  py-10 text-center col-span-5">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    className="w-10 py-4 mx-auto"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M1 11.27c0-.246.033-.492.099-.73l1.523-5.521A2.75 2.75 0 015.273 3h9.454a2.75 2.75 0 012.651 2.019l1.523 5.52c.066.239.099.485.099.732V15a2 2 0 01-2 2H3a2 2 0 01-2-2v-3.73zm3.068-5.852A1.25 1.25 0 015.273 4.5h9.454a1.25 1.25 0 011.205.918l1.523 5.52c.006.02.01.041.015.062H14a1 1 0 00-.86.49l-.606 1.02a1 1 0 01-.86.49H8.236a1 1 0 01-.894-.553l-.448-.894A1 1 0 006 11H2.53l.015-.062 1.523-5.52z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                  Your limit orders will appear here.
+                </td>) : (
+          activeOrdersSelected ? (
             <tbody className="divide-y divide-grey/70">
               {allLimitPositions.map((allLimitPosition) => {
                 if (allLimitPosition.positionId != undefined) {
@@ -1169,8 +1185,10 @@ export default function Trade() {
                 }
               })}
             </tbody>
+          )
           )}
         </table>
+        </div>
       </div>
     </div>
   );
