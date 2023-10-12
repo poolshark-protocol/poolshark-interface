64622182ccaf72843c46992e3ad66562a1d2b8de
0xnexusflip <0xnexusflip@gmail.com>
2023-10-12 17:57:06 +0100

    fix tr

--- src/pages/Trade.tsx
+++ src/pages/Trade.tsx
@@ -157,7 +157,7 @@ export default function Trade() {
 
   //log addresses and ids
   const [limitPoolAddressList, setLimitPoolAddressList] = useState([]);
-  const [limitPositionSnapshotList, setLimitPositionSnapshotList] = useState([]);
+  const [limitPositionSnapshotList, setLimitPositionSnapshotList] = useState<any[]>([]);
 
   //log amount in and out
   const [limitFilledAmountList, setLimitFilledAmountList] = useState([]);
@@ -348,6 +348,8 @@ export default function Trade() {
       if (allLimitPositions.length > 0) {
         for (let i = 0; i < allLimitPositions.length; i++) {
           mappedLimitPoolAddresses[i] = allLimitPositions[i].poolId;
+
+          mappedLimitSnapshotParams[i] = [];
 
           mappedLimitSnapshotParams[i][0] = address;
           mappedLimitSnapshotParams[i][1] = ethers.utils.parseUnits("1", 38);
@@ -1179,21 +1181,26 @@ export default function Trade() {
               <th className="text-left md:table-cell hidden pl-2 uppercase">Age</th>
             </tr>
           </thead>
-          {allLimitPositions.length === 0  ? (<td className="text-grey1 text-xs w-full  py-10 text-center col-span-5">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                    className="w-10 py-4 mx-auto"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M1 11.27c0-.246.033-.492.099-.73l1.523-5.521A2.75 2.75 0 015.273 3h9.454a2.75 2.75 0 012.651 2.019l1.523 5.52c.066.239.099.485.099.732V15a2 2 0 01-2 2H3a2 2 0 01-2-2v-3.73zm3.068-5.852A1.25 1.25 0 015.273 4.5h9.454a1.25 1.25 0 011.205.918l1.523 5.52c.006.02.01.041.015.062H14a1 1 0 00-.86.49l-.606 1.02a1 1 0 01-.86.49H8.236a1 1 0 01-.894-.553l-.448-.894A1 1 0 006 11H2.53l.015-.062 1.523-5.52z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
-                  Your limit orders will appear here.
-                </td>) : (
+          {allLimitPositions.length === 0  ? (
+          <tbody>
+            <tr>
+              <td className="text-grey1 text-xs w-full  py-10 text-center col-span-5">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                      className="w-10 py-4 mx-auto"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M1 11.27c0-.246.033-.492.099-.73l1.523-5.521A2.75 2.75 0 015.273 3h9.454a2.75 2.75 0 012.651 2.019l1.523 5.52c.066.239.099.485.099.732V15a2 2 0 01-2 2H3a2 2 0 01-2-2v-3.73zm3.068-5.852A1.25 1.25 0 015.273 4.5h9.454a1.25 1.25 0 011.205.918l1.523 5.52c.006.02.01.041.015.062H14a1 1 0 00-.86.49l-.606 1.02a1 1 0 01-.86.49H8.236a1 1 0 01-.894-.553l-.448-.894A1 1 0 006 11H2.53l.015-.062 1.523-5.52z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                    Your limit orders will appear here.
+              </td>
+            </tr>
+          </tbody>) : (
           activeOrdersSelected ? (
             <tbody className="divide-y divide-grey/70">
               {allLimitPositions.map((allLimitPosition, index) => {
@@ -1201,7 +1208,7 @@ export default function Trade() {
                   return (
                     <UserLimitPool
                       limitPosition={allLimitPosition}
-                      limitFilledAmount={parseFloat(ethers.utils.formatEther(limitFilledAmountList[index]))}
+                      limitFilledAmount={parseFloat(/*ethers.utils.formatEther(limitFilledAmountList[index])*/0)}
                       address={address}
                       href={"/limit/view"}
                       key={allLimitPosition.positionId}
