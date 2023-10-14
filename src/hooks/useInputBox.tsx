c39ee60e6d8353d3848efd89519c72ec9c40e357
alphak3y <alphak3y@protonmail.com>
2023-10-14 08:58:15 -0600

    cleanup input box

--- src/hooks/useInputBox.tsx
+++ src/hooks/useInputBox.tsx
@@ -6,10 +6,9 @@ import { token } from '../utils/types'
 
 export default function useInputBox() {
   const [display, setDisplay] = useState('')
-  const [input, setInput] = useState(BigNumber.from('0'))
   const [bnInput, setBnInput] = useState(BigNumber.from('0'))
 
-  const handleChange = (event, updateValue, tokenDecimals, handler?) => {
+  const handleChange = (event, tokenDecimals) => {
     tokenDecimals = tokenDecimals ?? 18
     const result = inputFilter(event.target.value)
     //TODO: do not allow for exceeding max decimals
@@ -17,32 +16,14 @@ export default function useInputBox() {
     if (result == '') {
       setBnInput(BN_ZERO)
     }
-    if (updateValue !== undefined) {
-      updateValue(result == '' ? 0 : result)
-      setInput(
-        result == ''
-          ? BN_ZERO
-          : ethers.utils.parseUnits(result, tokenDecimals),
-      )
-      if (result !== '') {
-        const valueToBn = ethers.utils.parseUnits(result, tokenDecimals)
-        setBnInput(valueToBn)
-      }
-    }
-    setInput(
-      result == '' ? BN_ZERO : ethers.utils.parseUnits(result, tokenDecimals),
-    )
     if (result !== '') {
       const valueToBn = ethers.utils.parseUnits(result, tokenDecimals)
       setBnInput(valueToBn)
     }
-    if (handler)
-      handler(inputFilter(event.target.value))
   }
 
   const maxBalance = (balance, placeholder, tokenDecimals) => {
     setDisplay(balance)
-    setInput(ethers.utils.parseUnits(balance.toString(), tokenDecimals))
     if (balance != '') {
       const valueToBn = ethers.utils.parseUnits(balance.toString(), tokenDecimals)
       setBnInput(valueToBn)
@@ -59,7 +40,7 @@ export default function useInputBox() {
           id="input"
           name={inputName ?? "input"}
           disabled={disabled ?? false}
-          onChange={(e) => handler ? handler(e) : handleChange(e, token.decimals, undefined)}
+          onChange={(e) => handler ? handler(e) : handleChange(e, token.decimals)}
           value={display}
           placeholder={placeholder}
           className="bg-transparent placeholder:text-grey1 w-full text-white text-3xl focus:ring-0 focus:ring-offset-0 focus:outline-none"
