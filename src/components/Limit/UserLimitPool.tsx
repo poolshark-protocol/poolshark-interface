779883c6375e41e8509a18b3b779bc2c4f799a7a
re traca <ggpt98@gmail.com>
2023-10-27 14:55:14 +0100

    better behaviour

--- src/components/Limit/UserLimitPool.tsx
+++ src/components/Limit/UserLimitPool.tsx
@@ -1,7 +1,11 @@
 import { useEffect } from "react";
 import { getClaimTick } from "../../utils/maps";
 import { BigNumber, ethers } from "ethers";
-import { getAveragePrice, getExpectedAmountOut, getExpectedAmountOutFromInput } from "../../utils/math/priceMath";
+import {
+  getAveragePrice,
+  getExpectedAmountOut,
+  getExpectedAmountOutFromInput,
+} from "../../utils/math/priceMath";
 import LimitSwapBurnButton from "../Buttons/LimitSwapBurnButton";
 import { tokenRangeLimit } from "../../utils/types";
 import router from "next/router";
@@ -13,165 +17,192 @@ import { useConfigStore } from "../../hooks/useConfigStore";
 import { invertPrice } from "../../utils/math/tickMath";
 
 export default function UserLimitPool({
-    limitPosition,
-    limitFilledAmount,
-    address,
-    href,
+  limitPosition,
+  limitFilledAmount,
+  address,
+  href,
 }) {
-    const [
-        limitSubgraph,
-        coverSubgraph
-    ] = useConfigStore((state) => [
-        state.limitSubgraph,
-        state.coverSubgraph
-    ]);
+  const [limitSubgraph] = useConfigStore((state) => [state.limitSubgraph]);
 
-    const [
-        tokenIn,
-        tokenOut,
-        setLimitPositionData,
-        setLimitPoolAddress,
-        setTokenIn,
-        setTokenOut,
-        setClaimTick,
-        setLimitPoolFromVolatility,
-        setNeedsAllowanceIn,
-        setNeedsBalanceIn,
-    ] = useRangeLimitStore((state) => [
-        state.tokenIn,
-        state.tokenOut,
-        state.setLimitPositionData,
-        state.setLimitPoolAddress,
-        state.setTokenIn,
-        state.setTokenOut,
-        state.setClaimTick,
-        state.setLimitPoolFromVolatility,
-        state.setNeedsAllowanceIn,
-        state.setNeedsBalanceIn,
-    ]);
+  const [
+    tokenIn,
+    tokenOut,
+    setLimitPositionData,
+    setLimitPoolAddress,
+    setTokenIn,
+    setTokenOut,
+    setClaimTick,
+    setLimitPoolFromVolatility,
+    setNeedsAllowanceIn,
+    setNeedsBalanceIn,
+  ] = useRangeLimitStore((state) => [
+    state.tokenIn,
+    state.tokenOut,
+    state.setLimitPositionData,
+    state.setLimitPoolAddress,
+    state.setTokenIn,
+    state.setTokenOut,
+    state.setClaimTick,
+    state.setLimitPoolFromVolatility,
+    state.setNeedsAllowanceIn,
+    state.setNeedsBalanceIn,
+  ]);
 
-    ///////////////////////////Claim Tick
-    useEffect(() => {
-        updateClaimTick();
-    }, [limitPosition]);
+  ///////////////////////////Claim Tick
+  useEffect(() => {
+    updateClaimTick();
+  }, [limitPosition]);
 
-    const updateClaimTick = async () => {
-        const tick = await getClaimTick(
-            limitPosition.poolId,
-            Number(limitPosition.min),
-            Number(limitPosition.max),
-            tokenIn.callId == 0,
-            Number(limitPosition.epochLast),
-            false,
-            limitSubgraph
-        );
-        setClaimTick(tick);
-    };
+  const updateClaimTick = async () => {
+    const tick = await getClaimTick(
+      limitPosition.poolId,
+      Number(limitPosition.min),
+      Number(limitPosition.max),
+      tokenIn.callId == 0,
+      Number(limitPosition.epochLast),
+      false,
+      limitSubgraph
+    );
+    setClaimTick(tick);
+  };
 
-    //////////////////////////Set Position when selected
+  //////////////////////////Set Position when selected
 
-    async function choosePosition() {
-        setLimitPositionData(limitPosition);
-        setNeedsAllowanceIn(true);
-        setNeedsBalanceIn(true);
-        const tokenInNew = {
-            name: limitPosition.tokenIn.name,
-            symbol: limitPosition.tokenIn.symbol,
-            logoURI: logoMap[limitPosition.tokenIn.symbol],
-            address: limitPosition.tokenIn.id,
-            decimals: limitPosition.tokenIn.decimals,
-        } as tokenRangeLimit;
-        const tokenOutNew = {
-            name: limitPosition.tokenOut.name,
-            symbol: limitPosition.tokenOut.symbol,
-            logoURI: logoMap[limitPosition.tokenOut.symbol],
-            address: limitPosition.tokenOut.id,
-            decimals: limitPosition.tokenOut.decimals,
-        } as tokenRangeLimit;
-        setTokenIn(tokenOutNew, tokenInNew, '0', true);
-        setTokenOut(tokenInNew, tokenOutNew, '0', false);
-        setLimitPoolFromVolatility(
-            tokenInNew,
-            tokenOutNew,
-            limitPosition.feeTier.toString(),
-            limitSubgraph,
-        );
-        router.push({
-            pathname: href,
-            query: {
-                positionId: limitPosition.positionId,
-            },
-        });
-    }
+  async function choosePosition() {
+    setLimitPositionData(limitPosition);
+    setNeedsAllowanceIn(true);
+    setNeedsBalanceIn(true);
+    const tokenInNew = {
+      name: limitPosition.tokenIn.name,
+      symbol: limitPosition.tokenIn.symbol,
+      logoURI: logoMap[limitPosition.tokenIn.symbol],
+      address: limitPosition.tokenIn.id,
+      decimals: limitPosition.tokenIn.decimals,
+    } as tokenRangeLimit;
+    const tokenOutNew = {
+      name: limitPosition.tokenOut.name,
+      symbol: limitPosition.tokenOut.symbol,
+      logoURI: logoMap[limitPosition.tokenOut.symbol],
+      address: limitPosition.tokenOut.id,
+      decimals: limitPosition.tokenOut.decimals,
+    } as tokenRangeLimit;
+    setTokenIn(tokenOutNew, tokenInNew, "0", true);
+    setTokenOut(tokenInNew, tokenOutNew, "0", false);
+    setLimitPoolFromVolatility(
+      tokenInNew,
+      tokenOutNew,
+      limitPosition.feeTier.toString(),
+      limitSubgraph
+    );
+    router.push({
+      pathname: href,
+      query: {
+        positionId: limitPosition.positionId,
+      },
+    });
+  }
 
-    return (
-        <tr className="text-right text-xs md:text-sm bg-black hover:bg-dark cursor-pointer"
-            key={limitPosition.positionId}
-            onClick={choosePosition}
-        >
-            <td className="py-3 pl-3">
-                <div className="flex items-center text-xs text-grey1 gap-x-2 text-left">
-                    <img
-                        className="w-[23px] h-[23px]"
-                        src={logoMap[limitPosition.tokenIn.symbol]}
-                    />
-                    {parseFloat(ethers.utils.formatUnits(limitPosition.amountIn, limitPosition.tokenIn.decimals)).toFixed(3) + " " + limitPosition.tokenIn.symbol}
-                </div>
-            </td>
-            <td className="">
-                <div className="flex items-center text-xs text-white gap-x-2 text-left">
-                    <img
-                        className="w-[23px] h-[23px]"
-                        src={logoMap[limitPosition.tokenOut.symbol]}
-                    />
-                    {parseFloat(ethers.utils.formatUnits(
-                        getExpectedAmountOut(
-                            parseInt(limitPosition.min),
-                            parseInt(limitPosition.max),
-                            limitPosition.tokenIn.id.localeCompare(limitPosition.tokenOut.id) < 0,
-                            BigNumber.from(limitPosition.liquidity)
-                        ),
-                        limitPosition.tokenOut.decimals
-                    )).toPrecision(6) + " " + limitPosition.tokenOut.symbol}
-                </div>
-            </td>
-            <td className="text-left text-xs">
-                <div className="flex flex-col">
-                    <span>
-                        <span className="text-grey1">1 {limitPosition.tokenIn.symbol} = </span>
-                        {
-                            invertPrice(getAveragePrice(
-                                limitPosition.tokenIn,
-                                limitPosition.tokenOut,
-                                parseInt(limitPosition.min),
-                                parseInt(limitPosition.max),
-                                limitPosition.tokenIn.id.localeCompare(limitPosition.tokenOut.id) < 0,
-                                BigNumber.from(limitPosition.liquidity),
-                                BigNumber.from(limitPosition.amountIn)
-                            ).toPrecision(6), limitPosition.tokenIn.id.localeCompare(limitPosition.tokenOut.id) < 0) + " " + limitPosition.tokenOut.symbol}
-                    </span>
-                </div>
-            </td>
-            <td className="md:table-cell hidden">
-                <div className="text-white bg-black border border-grey relative flex items-center justify-center h-7 rounded-[4px] text-center text-[10px]">
-                    <span className="z-50 px-3">
-                    {(limitFilledAmount /
-                         parseFloat(
-                           ethers.utils.formatUnits(
-                             getExpectedAmountOutFromInput(
-                               parseInt(limitPosition.min),
-                               parseInt(limitPosition.max),
-                               limitPosition.tokenIn.id.localeCompare(limitPosition.tokenOut.id) < 0,
-                               BigNumber.from(limitPosition.amountIn)
-                           ), limitPosition.tokenOut.decimals
-                         ))).toFixed(2)}% Filled
-                    </span>
-                    <div className="h-full bg-grey/60 w-[0%] absolute left-0" />
-                </div>
-            </td>
-            <td className="text-grey1 text-left pl-3 text-xs md:table-cell hidden">{timeDifference(limitPosition.timestamp)}</td>
-            <td className="text-sm text-grey1 md:table-cell hidden">
-                <LimitSwapBurnButton
+  return (
+    <tr
+      className="text-right text-xs md:text-sm bg-black hover:bg-dark cursor-pointer"
+      key={limitPosition.positionId}
+      onClick={choosePosition}
+    >
+      <td className="py-3 pl-3">
+        <div className="flex items-center text-xs text-grey1 gap-x-2 text-left">
+          <img
+            className="w-[23px] h-[23px]"
+            src={logoMap[limitPosition.tokenIn.symbol]}
+          />
+          {parseFloat(
+            ethers.utils.formatUnits(
+              limitPosition.amountIn,
+              limitPosition.tokenIn.decimals
+            )
+          ).toFixed(3) +
+            " " +
+            limitPosition.tokenIn.symbol}
+        </div>
+      </td>
+      <td className="">
+        <div className="flex items-center text-xs text-white gap-x-2 text-left">
+          <img
+            className="w-[23px] h-[23px]"
+            src={logoMap[limitPosition.tokenOut.symbol]}
+          />
+          {parseFloat(
+            ethers.utils.formatUnits(
+              getExpectedAmountOut(
+                parseInt(limitPosition.min),
+                parseInt(limitPosition.max),
+                limitPosition.tokenIn.id.localeCompare(
+                  limitPosition.tokenOut.id
+                ) < 0,
+                BigNumber.from(limitPosition.liquidity)
+              ),
+              limitPosition.tokenOut.decimals
+            )
+          ).toPrecision(6) +
+            " " +
+            limitPosition.tokenOut.symbol}
+        </div>
+      </td>
+      <td className="text-left text-xs">
+        <div className="flex flex-col">
+          <span>
+            <span className="text-grey1">
+              1 {limitPosition.tokenIn.symbol} ={" "}
+            </span>
+            {invertPrice(
+              getAveragePrice(
+                limitPosition.tokenIn,
+                limitPosition.tokenOut,
+                parseInt(limitPosition.min),
+                parseInt(limitPosition.max),
+                limitPosition.tokenIn.id.localeCompare(
+                  limitPosition.tokenOut.id
+                ) < 0,
+                BigNumber.from(limitPosition.liquidity),
+                BigNumber.from(limitPosition.amountIn)
+              ).toPrecision(6),
+              limitPosition.tokenIn.id.localeCompare(
+                limitPosition.tokenOut.id
+              ) < 0
+            ) +
+              " " +
+              limitPosition.tokenOut.symbol}
+          </span>
+        </div>
+      </td>
+      <td className="md:table-cell hidden">
+        <div className="text-white bg-black border border-grey relative flex items-center justify-center h-7 rounded-[4px] text-center text-[10px]">
+          <span className="z-50 px-3">
+            {(
+              limitFilledAmount /
+              parseFloat(
+                ethers.utils.formatUnits(
+                  getExpectedAmountOutFromInput(
+                    parseInt(limitPosition.min),
+                    parseInt(limitPosition.max),
+                    limitPosition.tokenIn.id.localeCompare(
+                      limitPosition.tokenOut.id
+                    ) < 0,
+                    BigNumber.from(limitPosition.amountIn)
+                  ),
+                  limitPosition.tokenOut.decimals
+                )
+              )
+            ).toFixed(2)}
+            % Filled
+          </span>
+          <div className="h-full bg-grey/60 w-[0%] absolute left-0" />
+        </div>
+      </td>
+      <td className="text-grey1 text-left pl-3 text-xs md:table-cell hidden">
+        {timeDifference(limitPosition.timestamp)}
+      </td>
+      <td className="text-sm text-grey1 md:table-cell hidden">
+        {/* <LimitSwapBurnButton
                     poolAddress={limitPosition.poolId}
                     address={address}
                     positionId={BigNumber.from(limitPosition.positionId)}
@@ -180,8 +211,8 @@ export default function UserLimitPool({
                     lower={BigNumber.from(limitPosition.min)}
                     upper={BigNumber.from(limitPosition.max)}
                     burnPercent={parseUnits("1", 38)}
-                />
-            </td>
-        </tr>
-    );
+                /> */}
+      </td>
+    </tr>
+  );
 }
