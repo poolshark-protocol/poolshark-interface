7fce36ec310f1732632adfeda7bc709615912863
lucasregueiroo <hello@lucasregueiro.com>
2023-11-07 02:49:26 +0000

    add size wanring on create range

--- src/pages/range/add-liquidity.tsx
+++ src/pages/range/add-liquidity.tsx
@@ -815,6 +815,14 @@ export default function AddLiquidity({}) {
                   the market price moves into your range.
                 </div>
               )}
+
+<div className=" text-red-600 bg-red-900/30 text-[10px] md:text-[11px] flex items-center md:gap-x-5 gap-x-3 p-2 rounded-[8px]">
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="md:w-9 w-12">
+  <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-5a.75.75 0 01.75.75v4.5a.75.75 0 01-1.5 0v-4.5A.75.75 0 0110 5zm0 10a1 1 0 100-2 1 1 0 000 2z" clipRule="evenodd" />
+</svg>
+
+                Please increase the selected size or price range in order to create a position
+                </div>
             </div>
           </div>
         </div>
