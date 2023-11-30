4992822bcf9cc0b7eae3dada4f748e82e321d58c
0xnexusflip <0xnexusflip@gmail.com>
2023-11-30 15:43:40 +0000

    comment out bond notification pop up

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,7 @@ export default function Home() {
       <div className="flex justify-center w-full text-white">
         <Trade />
       </div>
-      <Notification />
+      {/*<Notification />*/}
     </div>
   );
 }
