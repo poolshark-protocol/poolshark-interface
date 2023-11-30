9b7e2d3cd0ea7cf72713279306ec593e1dd43bab
0xnexusflip <0xnexusflip@gmail.com>
2023-11-30 11:31:38 +0000

    add amount exceeds max deposit check

--- src/pages/bond/index.tsx
+++ src/pages/bond/index.tsx
@@ -469,19 +469,27 @@ export default function Bond() {
                       ""}</span>
                 </div>
               </div>
-              {tokenAllowance >= bnInput ?
-                <BuyBondButton
-                  inputAmount={bnInput}
-                  minAmount={bnInput}
-                  setNeedsSubgraph={setNeedsSubgraph}
-                  setNeedsBalance={setNeedsBalance}
-                  setNeedsAllowance={setNeedsAllowance}
-                  marketId={BigNumber.from(46)}
-                /> :
-                <ApproveBondButton
-                  inputAmount={bnInput}
-                  setNeedsAllowance={setNeedsAllowance}
-                />}
+              {parseFloat(formatEther(bnInput)) <= maxAmountAccepted ?
+                (tokenAllowance >= bnInput ?
+                  <BuyBondButton
+                    inputAmount={bnInput}
+                    minAmount={bnInput}
+                    setNeedsSubgraph={setNeedsSubgraph}
+                    setNeedsBalance={setNeedsBalance}
+                    setNeedsAllowance={setNeedsAllowance}
+                    marketId={BigNumber.from(46)}
+                  /> :
+                  <ApproveBondButton
+                    inputAmount={bnInput}
+                    setNeedsAllowance={setNeedsAllowance}
+                  />) :
+                  <button
+                    className="w-full py-4 mx-auto disabled:cursor-not-allowed cursor-pointer flex items-center justify-center text-center transition rounded-full  border border-main bg-main1 uppercase text-sm disabled:opacity-50 hover:opacity-80"
+                    disabled={true}
+                  >
+                    AMOUNT EXCEEDS MAX DEPOSIT
+                  </button>
+                }
             </div>
           </div>
           </div>
