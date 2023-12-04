9f9e446ac821361e9e3bfd4e46aa30fed97ab034
lucasregueiroo <hello@lucasregueiro.com>
2023-12-04 17:04:26 +0000

    add blocked page

---
+++ src/pages/blocked.tsx
@@ -0,0 +1,10 @@
+import Trade from "./Trade";
+import Navbar from "../components/Navbar";
+
+export default function Home() {
+  return (
+    <div className=" bg-no-repeat bg-black min-h-screen ">
+      testing
+    </div>
+  );
+}
