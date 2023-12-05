736f5a55221fcf5b2552f91710aeb763c6f3c691
lucasregueiroo <hello@lucasregueiro.com>
2023-12-05 13:17:10 +0000

    fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,7 @@ import { NextRequest, NextResponse } from 'next/server';
 export function middleware(req: NextRequest) {
   const { geo } = req;
 
-  if (geo.country === "US") {
+  if (geo.country === "US" && !req.nextUrl.pathname.startsWith('/blocked')) {
     // Redirect users from the US to the "/blocked" page
     return NextResponse.redirect('https://poolshark-interface-git-geoblocking-poolshark.vercel.app/blocked');
   }
