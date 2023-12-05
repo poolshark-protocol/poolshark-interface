4aef930c097912ce88bac0e1a16acee692f5d58b
lucasregueiroo <hello@lucasregueiro.com>
2023-12-05 15:50:41 +0000

    add ToS

--- src/components/Buttons/ConnectWalletButton.tsx
+++ src/components/Buttons/ConnectWalletButton.tsx
@@ -3,6 +3,7 @@ import { ChevronDownIcon } from "@heroicons/react/20/solid";
 import Network from "../Modals/Network";
 import { useState } from "react";
 import React from "react";
+import { useEffect } from "react";
 
 interface Props {
     xl?: boolean;
@@ -12,6 +13,23 @@ interface Props {
 
 export const ConnectWalletButton = ({xl= false, center= false}: Props) => {
   const [isOpen, setIsOpen] = useState(false);
+  const [tosAccepted, setTosAccepted] = useState(false);
+
+  useEffect(() => {
+    // Check if terms of service is accepted
+    const isTosAccepted = localStorage.getItem('tosAccepted') === 'true';
+    setTosAccepted(isTosAccepted);
+
+    // Simulate wallet connection logic
+    // In real scenario, this will be replaced with actual wallet connection logic
+    // setWalletConnected(true/false) based on wallet connection status
+}, []);
+
+const handleTosAccept = () => {
+    localStorage.setItem('tosAccepted', 'true');
+    setTosAccepted(true);
+};
+
 
   return (
     <>
--- src/pages/blocked.tsx
+++ src/pages/blocked.tsx
@@ -2,7 +2,7 @@
 
 export default function Home() {
   return (
-    <div className="bg-black h-screen w-screen flex items-center justify-center text-white">
+    <div className="bg-black h-screen w-screen flex items-center justify-center text-white px-5">
       <div className="bg-dark border border-grey p-4 max-w-lg rounded-[4px]">
         <h1>Access Restricted</h1>
         <p className="text-white/60 text-sm mt-2 mb-4">Unfortunately, the country you are connecting from is not supported.</p>
