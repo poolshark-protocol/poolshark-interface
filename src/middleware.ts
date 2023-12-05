3dd86cf26218867e0a42d475d775f98bf5d0a2eb
lucasregueiroo <hello@lucasregueiro.com>
2023-12-05 13:56:57 +0000

    fix css

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,16 +3,18 @@ import { NextRequest, NextResponse } from 'next/server';
 export function middleware(req: NextRequest) {
   const { geo } = req;
 
+  if ( req.nextUrl.pathname.startsWith("/_next")) {
+    return NextResponse.next()
+  } else {
+
   if (geo.country === "US" && !req.nextUrl.pathname.startsWith('/blocked')) {
     // Redirect users from the US to the "/blocked" page
     return NextResponse.redirect('https://poolshark-interface-git-geoblocking-poolshark.vercel.app/blocked');
+  } else {
+    return NextResponse.next()
   }
 
-  // Continue with the request for users from other countries
-  if ( req.nextUrl.pathname.startsWith("/_next")) {
-    return NextResponse.next()}
-
-    return NextResponse.next()
+  }
 
 
 }
