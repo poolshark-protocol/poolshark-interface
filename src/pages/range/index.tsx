2d15a87da6a08a0687b13cd92bb916636c35b00e
alphak3y <alphak3y@protonmail.com>
2023-12-19 09:41:50 -0700

    default tick spacing

--- src/pages/range/index.tsx
+++ src/pages/range/index.tsx
@@ -158,7 +158,7 @@ export default function Range() {
                   router.push({
                     pathname: "/range/add-liquidity",
                     query: {
-                      feeTier: allRangePools[0].feeTier ?? 10,
+                      feeTier: allRangePools[0].feeTier ?? 3000,
                       poolId: allRangePools[0].poolId,
                     },
                   });
