131c76b0d2c94e69f31f1c11dda697d83d557482
alphak3y <alphak3y@protonmail.com>
2024-01-23 15:50:17 -0700

    bump next version to 13

--- src/hooks/toasts.tsx
+++ src/hooks/toasts.tsx
@@ -2,16 +2,18 @@ import { ToastProvider } from 'rc-toastr'
 import * as ReactDOM from 'react-dom';
 import "rc-toastr/dist/index.css" // import the css file
 import MyApp from '../pages/_app';
+import { createRoot } from 'react-dom/client';
 
+const container = document.getElementById('root');
 
-//review that I added this properly
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
 
-ReactDOM.render((
+root.render((
     <ToastProvider config={{
         position: "top-right",
         duration: 3000
     }} >
         <MyApp Component={undefined} pageProps={undefined} />
     </ToastProvider>
-), document.getElementById('root'))
+))
 
