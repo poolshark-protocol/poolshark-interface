db39bea20c635740cd7b2aa1656cfb4050b668aa
lucasregueiroo <hello@lucasregueiro.com>
2024-01-29 15:49:07 +0000

    fix small styling issues

--- src/components/Trade/LimitSwap.tsx
+++ src/components/Trade/LimitSwap.tsx
@@ -892,7 +892,15 @@ export default function LimitSwap() {
           </div>
         </div>
       </div>
-      <div className="flex items-center justify-center w-full pt-7 pb-4">
+      <div 
+      onClick={() => {
+        switchDirection(
+          exactIn,
+          exactIn ? displayIn : displayOut,
+          exactIn ? setAmountIn : setAmountOut
+        );
+      }}
+      className="flex items-center justify-center w-full pt-10 pb-3">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
@@ -900,13 +908,7 @@ export default function LimitSwap() {
           strokeWidth="1.5"
           stroke="currentColor"
           className="w-5 cursor-pointer"
-          onClick={() => {
-            switchDirection(
-              exactIn,
-              exactIn ? displayIn : displayOut,
-              exactIn ? setAmountIn : setAmountOut
-            );
-          }}
+          
         >
           <path
             strokeLinecap="round"
