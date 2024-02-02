0dd6db67516f2164e5354328ac1929718042554a
re traca <ggpt98@gmail.com>
2024-02-02 13:05:59 -0300

    display when token usd price not fetched or 0

--- src/components/Display/USDPriceDisplay.tsx
+++ src/components/Display/USDPriceDisplay.tsx
@@ -1,16 +1,13 @@
-import { ZERO_ADDRESS } from "../../utils/math/constants";
-import { numFormat } from "../../utils/math/valueMath";
-
 function USDPriceDisplay({ token, display }) {
   return (
     <span>
       ~$
-      {!isNaN(token.decimals) && !isNaN(token.USDPrice)
+      {!isNaN(token.decimals) && !isNaN(token.USDPrice) && token.USDPrice != "0"
         ? (
             (!isNaN(parseFloat(display)) ? parseFloat(display) : 0) *
             (token.USDPrice ?? 0)
           ).toFixed(2)
-        : (0).toFixed(2)}
+        : "-.--"}
     </span>
   );
 }
