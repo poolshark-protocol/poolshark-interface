a6385845f3f5e29c81e4dfef56a84b9a36c17eb6
Tawnee <allprt89@gmail.com>
2024-03-08 09:46:28 -0700

    fix convert function types

--- src/utils/misc.ts
+++ src/utils/misc.ts
@@ -12,10 +12,7 @@ export function useCopyElementUseEffect(event, setCopied) {
 
 export function convertBigIntAndBigNumber(input: bigint): BigNumber;
 export function convertBigIntAndBigNumber(input: BigNumber): bigint;
-export function convertBigIntAndBigNumber(input: undefined): undefined;
 export function convertBigIntAndBigNumber(input) {
-  if (input === undefined || input === null) return;
-
   if (BigNumber.isBigNumber(input)) {
     return BigInt(input.toString());
   }
@@ -33,22 +30,33 @@ type ConvertBigIntAndBigNumber<T> = T extends BigNumber
   ? { [K in keyof T]: ConvertBigIntAndBigNumber<T[K]> } // Recurse into objects
   : T; // Leave other types unchanged
 
-
-export function deepConvertBigIntAndBigNumber<T>(obj: T): ConvertBigIntAndBigNumber<T> {
-  if (BigNumber.isBigNumber(obj) || typeof obj === 'bigint') {
-    // Direct conversion for BigNumber or bigint values
-    return convertBigIntAndBigNumber(obj);
-  } else if (Array.isArray(obj)) {
-    // Recursively handle arrays
-    return obj.map(deepConvertBigIntAndBigNumber);
-  } else if (obj !== null && typeof obj === 'object') {
-    // Recursively handle objects
-    const convertedObject: Record<string, any> = {};
-    for (const [key, value] of Object.entries(obj)) {
-      convertedObject[key] = deepConvertBigIntAndBigNumber(value);
-    }
-    return convertedObject;
+export function deepConvertBigIntAndBigNumber<T>(value: T): ConvertBigIntAndBigNumber<T> {
+  // Direct conversion for BigNumbers
+  if (BigNumber.isBigNumber(value)) {
+    return convertBigIntAndBigNumber(value) as ConvertBigIntAndBigNumber<T>;
   }
+
+  // Direct conversion for bigints
+  if (typeof value === 'bigint') {
+    return convertBigIntAndBigNumber(value) as ConvertBigIntAndBigNumber<T>;
+  }
+
+  // Recursively handle arrays
+  if (Array.isArray(value)) {
+    return value.map(
+      item => deepConvertBigIntAndBigNumber(item)
+    ) as unknown as ConvertBigIntAndBigNumber<T>;
+  }
+  
+  // Recursively handle objects
+  if (value !== null && typeof value === 'object') {
+    const convertedObject: Record<string, any> = {};
+    for (const [key, val] of Object.entries(value)) {
+      convertedObject[key] = deepConvertBigIntAndBigNumber(val);
+    }
+    return convertedObject as ConvertBigIntAndBigNumber<T>;
+  }
+
   // Return the value unchanged if it's not an object, array, BigNumber, or bigint
-  return obj;
+  return value as ConvertBigIntAndBigNumber<T>;
 }
