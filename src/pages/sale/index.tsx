1f463edffc7cb7738b4511cad6fb3541c6cb9533
alphak3y <alphak3y@protonmail.com>
2024-03-18 10:00:29 -0600

    typing fix

--- src/pages/sale/index.tsx
+++ src/pages/sale/index.tsx
@@ -105,7 +105,7 @@ export default function Sale() {
   }, [ethUsdPrice]);
 
   const { data: filledAmount } = useContractRead({
-    address: saleConfig.poolAddress,
+    address: saleConfig.poolAddress as `0x${string}`,
     abi: limitPoolABI,
     functionName: "snapshotLimit",
     args: [
