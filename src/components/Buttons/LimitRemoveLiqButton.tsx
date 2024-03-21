de37d543ecbc9d7753fa9015a6df540d1421dffe
alphak3y <alphak3y@protonmail.com>
2024-03-21 12:59:44 -0600

    enable button

--- src/components/Buttons/LimitRemoveLiqButton.tsx
+++ src/components/Buttons/LimitRemoveLiqButton.tsx
@@ -189,13 +189,13 @@ export default function LimitRemoveLiqButton({
   return (
     <>
       <button
-        disabled={gasLimit.lte(BN_ZERO) || claimTick == undefined}
+        disabled={claimTick == undefined}
         className="w-full py-4 mx-auto disabled:cursor-not-allowed cursor-pointer text-center flex items-center justify-center transition rounded-full  border border-main bg-main1 uppercase text-sm disabled:opacity-50 hover:opacity-80"
         onClick={() => {
           address ? write?.() : null;
         }}
       >
-        {gasLimit.lte(BN_ZERO) ? <Loader /> : "Remove liquidity"}
+        {"Remove liquidity"}
       </button>
     </>
   );
