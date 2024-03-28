eba71e21e39343ef0078abc8b49a5f0dc60844a8
Tawnee <tawnee.a@proton.me>
2024-03-28 11:58:47 -0600

    move setTimeouts to useeffect and clear them on unmount

--- src/pages/Trade.tsx
+++ src/pages/Trade.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, Fragment } from "react";
+import { useState, useEffect, Fragment, useRef } from "react";
 import { erc20ABI, useAccount, useContractRead, useBalance } from "wagmi";
 import { BigNumber, ethers } from "ethers";
 import { chainProperties } from "../utils/chains";
@@ -234,9 +234,6 @@ export default function Trade() {
     chainId: chainId,
     onSuccess(data) {
       tradeStore.setNeedsBalanceIn(false);
-      setTimeout(() => {
-        tradeStore.setNeedsBalanceIn(true);
-      }, 5000);
     },
   });
 
@@ -249,11 +246,26 @@ export default function Trade() {
     chainId: chainId,
     onSuccess(data) {
       tradeStore.setNeedsBalanceOut(false);
-      setTimeout(() => {
-        tradeStore.setNeedsBalanceOut(true);
-      }, 5000);
     },
   });
+
+  useEffect(() => {
+    if (!tradeStore.needsBalanceOut) {
+      const timer = setTimeout(() => {
+        tradeStore.setNeedsBalanceOut(true);
+      }, 5000);
+      return () => clearTimeout(timer);
+    }
+  }, [tradeStore.needsBalanceOut]);
+
+  useEffect(() => {
+    if (!tradeStore.needsBalanceIn) {
+      const timer = setTimeout(() => {
+        tradeStore.setNeedsBalanceIn(true);
+      }, 5000);
+      return () => clearTimeout(timer);
+    }
+  }, [tradeStore.needsBalanceIn]);
 
   useEffect(() => {
     if (isConnected && tokenInBal) {
